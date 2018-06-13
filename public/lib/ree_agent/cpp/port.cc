4e93d50390fcd81f968f2d4709267d2a3f267e5e
SY Chiu <m943040028@gmail.com>
2018-06-13 12:26:14 +0800

    [gzos] Implement tipc connect
    Change-Id: I21f588a2cf3bbc74e531496db56f8d5ed82c96af

--- public/lib/ree_agent/cpp/port.cc
+++ public/lib/ree_agent/cpp/port.cc
@@ -3,12 +3,53 @@
 // found in the LICENSE file.
 
 #include "lib/ree_agent/cpp/port.h"
+#include "lib/ree_agent/cpp/channel.h"
+#include "lib/ree_agent/cpp/object_manager.h"
 
 namespace ree_agent {
 
-void TipcPortImpl::OnConnectionRequest(
-    fidl::InterfaceRequest<TipcChannel> channel) {
-  callback_(std::move(channel));
+void TipcPortImpl::Connect(fidl::InterfaceHandle<TipcChannel> peer_handle,
+                           ConnectCallback callback) {
+  fbl::RefPtr<TipcChannelImpl> channel;
+  zx_status_t err = TipcChannelImpl::Create(num_items_, item_size_, &channel);
+  if (err != ZX_OK) {
+    FXL_LOG(ERROR) << "Failed to create channel object " << err;
+    callback(err, nullptr);
+  }
+
+  channel->BindPeerInterfaceHandle(std::move(peer_handle));
+  pending_requests_.push_front(channel);
+
+  err = SignalEvent(TipcEvent::READY);
+  FXL_DCHECK(err == ZX_OK);
+
+  auto local_handle = channel->GetInterfaceHandle();
+  callback(ZX_OK, std::move(local_handle));
+}
+
+void TipcPortImpl::GetInfo(GetInfoCallback callback) {
+  callback(num_items_, item_size_);
+}
+
+zx_status_t TipcPortImpl::Accept(fbl::RefPtr<TipcChannelImpl>* channel_out) {
+  FXL_DCHECK(channel_out);
+  if (pending_requests_.is_empty()) {
+    return ZX_ERR_SHOULD_WAIT;
+  }
+  auto channel = pending_requests_.pop_front();
+
+  // TODO(sy): send the connection response message here
+  uint32_t dummy;
+  zx_status_t status = channel->SendMessage(&dummy, sizeof(dummy));
+  if (status != ZX_OK) {
+    return status;
+  }
+
+  zx_status_t err = TipcObjectManager::Instance()->InstallObject(channel);
+  FXL_CHECK(err == ZX_OK);
+
+  *channel_out = fbl::move(channel);
+  return ZX_OK;
 }
 
 }  // namespace ree_agent
